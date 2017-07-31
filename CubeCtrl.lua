2ba588e9e376cf03d118521d780d3505625af943
蔡华 <huacai@outlook.com>
2017-07-31 23:38:13 +0800

    记不清楚改了啥

---
+++ CubeCtrl.lua
@@ -0,0 +1,10 @@
+CubeCtrl = {}
+
+CubeCtrl.a = 1
+CubeCtrl.b = 1
+ 
+function CubeCtrl.foo()
+    return 3
+end
+
+return CubeCtrl
---
+++ Main.lua
@@ -0,0 +1,12 @@
+require ("CubeCtrl")
+
+Main = {}
+
+Main.a= 1
+Main.b= 1
+ 
+function Main.foo()
+    return "main"
+end
+
+print(CubeCtrl.a)
